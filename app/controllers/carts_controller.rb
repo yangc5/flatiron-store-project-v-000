861499c188a9dc972b58d26cd9833f17a0453de8
Chen Yang <yangc5@mail.uc.edu>
2016-02-28 17:30:58 -0600

    working on cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,13 @@
 class CartsController < ApplicationController
+  helper_method :current_cart
+
   def add_to_cart
+  end
+
+  def show
+  end
+
+  def current_cart
+    current_user.current_cart
   end
 end
